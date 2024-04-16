27cd5068156e891f4ccb58d2a730c5254afff39f
prabhasreddy-57 <faqwritetothismail@gmail.com>
2024-04-16 11:01:49 +0530

    docker

--- backend/product-management/seed.js
+++ backend/product-management/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Product } = require('./model'); // Adjust the path according to your structure
-
-mongoose.connect('mongodb://localhost:27017/productManagement', {
+require('dotenv').config()
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/productManagement', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB Connected'))
--- my-app/pages/products.js
+++ my-app/pages/products.js
@@ -6,24 +6,33 @@ export default function Home() {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('http://localhost:3002/products');
-      const data = await response.json();
-      setProducts(data);
+      try {
+        const response = await fetch('http://localhost:3002/products');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
     };
 
     fetchProducts();
   }, []);
 
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="text-2xl font-bold text-center my-6">Product List</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="container mx-auto px-4 py-8 h-screen">
+      <h1 className="text-3xl font-bold text-center mb-8">Product List</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
           <Link key={product._id} href={`/product/${product._id}`}
-             className="block border rounded-md p-4 hover:bg-gray-100">
-              <img src={product.image} alt={product.name} className="w-24 h-24 mx-auto" />
-              <h2 className="mt-2 text-lg font-semibold text-gray-900">{product.name}</h2>
-              <p className="text-gray-600">Price: ${product.price}</p>
+            className="border-2 border-sky-500 rounded-lg p-6 flex flex-col justify-between hover:shadow-lg transition duration-300">
+              <div>
+                <img src={product.image} alt={product.name} className="w-full h-20 object-cover mb-4" />
+                <h2 className="text-lg font-semibold text-gray-800">{product.name}</h2>
+                <p className="text-gray-600 mt-2">Price: ${product.price.toFixed(2)}</p>
+              </div>
+              <button className="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 hover:bg-blue-600 transition duration-300">
+                View Details
+              </button>
             
           </Link>
         ))}

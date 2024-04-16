d5399a1d1157f36064f357ae459af87aaa834ebf
prabhasreddy-57 <faqwritetothismail@gmail.com>
2024-04-16 09:47:22 +0530

    changes2

--- my-app/pages/Register.js
+++ my-app/pages/Register.js
@@ -4,7 +4,7 @@ function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-
+  const[username,setUsername]=useState("")
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (password !== confirmPassword) {
@@ -18,7 +18,7 @@ function Register() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email,username, password }),
       });
       const data = await response.json();
       if (response.ok) {
@@ -48,6 +48,19 @@ function Register() {
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             required
           />
         </div>
